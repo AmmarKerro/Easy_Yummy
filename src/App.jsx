17b8f28a6c285990125becfabb243852c9893d88
AmmarKerro <ammar.kerrouchi@gmail.com>
2024-07-17 17:00:22 +0200

    ✨ LoginPage: add Title and Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
-import { useState } from "react";
-
-import "./App.css";
 import LoginPage from "./components/pages/LoginPage";
 
-function App() {
-  const [name, setName] = useState("");
+import "./App.css";
 
-  const handleClicK = () => {
-    if (name.length > 0) alert(`Bonjour ${name}`);
-  };
+function App() {
   return <LoginPage />;
 }
 
--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,3 +1,32 @@
+import { useState } from "react";
+
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  const [name, setName] = useState("");
+  // const handleClicK = () => {
+  //   if (name.length > 0) alert(`Bonjour ${name}`);
+  // };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Bonjour ${name}`);
+    setName("");
+  };
+
+  return (
+    <div>
+      <h1>Bienvenue chez nous !</h1>
+      <br />
+      <h3>Connectez-vous</h3>
+      <form action="submit" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Entrez votre prénom..."
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        ></input>
+        <button type="submit">Accéder à votre espace</button>
+      </form>
+    </div>
+  );
 }
