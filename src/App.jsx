beb94819d4005188d7ce41b7ea4c275e0d237497
AmmarKerro <ammar.kerrouchi@gmail.com>
2024-07-19 19:20:26 +0200

    ✨ OrderPage: install routing system

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import LoginPage from "./components/pages/login/LoginPage";
 
 import "./App.css";
+import { Routes } from "react-router-dom";
+import ErrorPage from "./components/pages/error/ErrorPage";
 
 function App() {
-  return <LoginPage />;
+  return (
+    <Routes>
+      <Routes path="/" elements={<LoginPage />} />
+      <Routes path="/order" elements={<OrderPage />} />
+      <Routes path="*" elements={<ErrorPage />} />
+    </Routes>
+  );
 }
 
 export default App;
