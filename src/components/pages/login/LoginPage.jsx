17f3d289d3cb51335a1719fad5a9059aa19c120f
AmmarKerro <ammar.kerrouchi@gmail.com>
2024-07-24 13:17:46 +0200

    💄 LoginStyle: add style Login

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -12,10 +12,24 @@ export default function LoginPage() {
 }
 
 const LoginPageStyled = styled.div`
-  background-color: red;
   height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
+
+  ::before {
+    content: "";
+    background: url("/images/burger-background.jpg") rgba(0, 0, 0, 0.7);
+    background-size: cover;
+    background-position: center;
+    background-blend-mode: darken;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+  }
 `;
